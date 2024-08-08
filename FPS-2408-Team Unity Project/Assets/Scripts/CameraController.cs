0b3495058244e9791a8047b0a8043ab55511d0e6
Puddle <Puddleglumstudio@gmail.com>
2024-08-08 10:08:12 -0400

    Deleted Sample scene
    Sample scene was broken and outdated, removed from project

--- FPS-2408-Team Unity Project/Assets/Scripts/CameraController.cs
+++ FPS-2408-Team Unity Project/Assets/Scripts/CameraController.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Burst.CompilerServices;
-using Unity.VisualScripting;
 using UnityEditor.SearchService;
 using UnityEngine;
 
