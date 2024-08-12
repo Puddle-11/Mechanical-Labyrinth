2a8924f0041ab91c941ae48a3afa1764b631ce97
Puddle <Puddleglumstudio@gmail.com>
2024-08-11 20:18:43 -0400

    Minor updates, Fixed selector (mostly)
    finished LockOnDisplay
    added a couple new methods
    fixed low res textures by turning off compression
    updated targets to be small cylinders instead of spheres
    Imported Unity Sprite 2d Essentials (for selector)

--- FPS-2408-Team Unity Project/Assets/Scripts/Player/LockOnDisplay.cs
+++ FPS-2408-Team Unity Project/Assets/Scripts/Player/LockOnDisplay.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class LockOnDisplay : MonoBehaviour
 {
     public GameObject LockOnGUI;
-    [SerializeField] private Vector3 relativeScale;
+    [SerializeField] private float relativeScale;
+
+    [SerializeField] private Vector2 minSize;
+    [SerializeField] private float boundSize;
+
+ 
+
+    struct ssBounds
+    {
+       public Vector3 min;
+        public Vector3 max;
+    }
+    struct wsBounds
+    {
+        public Vector3 min;
+        public Vector3 max;
+    }
     public void Update()
     {
         CheckLOS();
@@ -18,13 +34,88 @@ public class LockOnDisplay : MonoBehaviour
             if(checkLineOfSight.collider.GetComponent<IHealth>() != null || checkLineOfSight.collider.GetComponent<Pickup>() != null)
             {
                 LockOnGUI.SetActive(true);
+                float distanceScale = relativeScale / Vector3.Distance(checkLineOfSight.collider.transform.position, Camera.main.transform.position);
                 Vector3 screenPos = Camera.main.WorldToScreenPoint(checkLineOfSight.collider.transform.position);
+                Renderer rendRef = checkLineOfSight.collider.GetComponent<Renderer>();
+                if(rendRef == null)
+                {
+                    rendRef = checkLineOfSight.collider.GetComponentInChildren<Renderer>();
+                }
+                if(rendRef != null)
+                {
+                    ssBounds objectScreenBounds = worldToScreenBounds(rendRef.bounds);
+
+                    Vector2 WH = new Vector2(objectScreenBounds.max.x - objectScreenBounds.min.x, objectScreenBounds.max.y - objectScreenBounds.min.y);
+                    RectTransform rectRef = LockOnGUI.GetComponent<RectTransform>();
+                    rectRef.sizeDelta = WH / distanceScale * boundSize;
+                  
+                }
+
+
                 LockOnGUI.transform.position = screenPos;
-                LockOnGUI.transform.localScale = relativeScale / Vector3.Distance(checkLineOfSight.collider.transform.position, Camera.main.transform.position);
+
+
+
+                LockOnGUI.transform.localScale = Vector3.one * distanceScale;
+
                 return;
             }
         }
         LockOnGUI.SetActive(false);
 
     }
+    private ssBounds worldToScreenBounds(Bounds _boundingBox)
+    {
+        wsBounds temp = new wsBounds();
+        temp.max = _boundingBox.max;
+        temp.min = _boundingBox.min;
+        return worldToScreenBounds(temp);
+    }
+    private ssBounds worldToScreenBounds(wsBounds _worldBounds)
+    {
+
+        Vector3[] screenPosCorners = new Vector3[8];
+        screenPosCorners[0] = Camera.main.WorldToScreenPoint(_worldBounds.max);
+
+        screenPosCorners[1] = Camera.main.WorldToScreenPoint(new Vector3(_worldBounds.min.x, _worldBounds.max.y, _worldBounds.max.z));
+        screenPosCorners[2] = Camera.main.WorldToScreenPoint(new Vector3(_worldBounds.min.x, _worldBounds.max.y, _worldBounds.min.z));
+        screenPosCorners[3] = Camera.main.WorldToScreenPoint(new Vector3(_worldBounds.max.x, _worldBounds.max.y, _worldBounds.min.z));
+
+        screenPosCorners[4] = Camera.main.WorldToScreenPoint(new Vector3(_worldBounds.min.x, _worldBounds.min.y, _worldBounds.max.z));
+        screenPosCorners[5] = Camera.main.WorldToScreenPoint(new Vector3(_worldBounds.max.x, _worldBounds.min.y, _worldBounds.max.z));
+        screenPosCorners[6] = Camera.main.WorldToScreenPoint(new Vector3(_worldBounds.max.x, _worldBounds.min.y, _worldBounds.min.z));
+        screenPosCorners[7] = Camera.main.WorldToScreenPoint(_worldBounds.min);
+
+
+        ssBounds result = new ssBounds();
+        result.min = screenPosCorners[0];
+
+        result.max = screenPosCorners[0];
+
+        for (int i = 0; i < 8; i++)
+        {
+            //new min for x found
+            if (screenPosCorners[i].x < result.min.x)
+            {
+                result.min.x = screenPosCorners[i].x;
+            }
+            //new max for x found
+            if (screenPosCorners[i].x > result.max.x)
+            {
+                result.max.x = screenPosCorners[i].x;
+            }
+
+            if (screenPosCorners[i].y < result.min.y)
+            {
+                result.min.y = screenPosCorners[i].y;
+            }
+            //new max for x found
+            if (screenPosCorners[i].y > result.max.y)
+            {
+                result.max.y = screenPosCorners[i].y;
+            }
+        }
+        return result;
+    }
+   
 }
