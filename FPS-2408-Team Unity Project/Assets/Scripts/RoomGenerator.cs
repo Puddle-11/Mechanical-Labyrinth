275039852581784e730e07943f925317aa2c533d
Puddle <Puddleglumstudio@gmail.com>
2024-08-21 21:16:05 -0400

    Completed level generation

--- FPS-2408-Team Unity Project/Assets/Scripts/RoomGenerator.cs
+++ FPS-2408-Team Unity Project/Assets/Scripts/RoomGenerator.cs
@@ -73,10 +73,12 @@ public class RoomGenerator : IGenerator
         //================================
         //Draw Rooms
         RoomMarker[] Rooms = GeneratePrimaryRooms(roomTexture);
+        
+
+
         for (int i = 0; i < Rooms.Length; i++)
         {
             Vector2Int offset = new Vector2Int((Rooms[i].R_Size.x / 2), (Rooms[i].R_Size.y / 2));
-            Debug.Log(offset);
             for (int x = 0; x < Rooms[i].R_Size.x; x++)
             {
                 for (int y = 0; y < Rooms[i].R_Size.y; y++)
@@ -88,32 +90,13 @@ public class RoomGenerator : IGenerator
 
        
         //================================
+
+        GenerateSecondaryRooms(roomTexture, 2, 100);
+
         for (int i = 0; i < Rooms.Length; i++)
         {
-            int nextRoom = i == Rooms.Length - 1? 0 : i +1;
-
-            for (int j = 0; j < 2; j++)
-            {
-
-                DrawLine(Rooms[i].R_Pos, Rooms[j].R_Pos, DoorWidth);
-            }
-
-            // DrawLine(Rooms[i].R_Pos, Rooms[nextRoom].R_Pos, DoorWidth);
-        }
-
-        GenerateSecondaryRooms(roomTexture);
-           //================================
-        //Set Outline To Black
-        for (int x = 0; x < roomTexture.Size().x; x++)
-        {
-            for (int y = 0; y < roomTexture.Size().y; y++)
-            {
-                if (x == 0 || x == roomTexture.Size().x - 1 || y == 0 || y == roomTexture.Size().y - 1)
-                {
-                    roomTexture.SetPixel(x, y, Color.black);
-                    continue;
-                }
-            }
+            int nextRoom = i == Rooms.Length - 1 ? 0 : i + 1;
+            DrawDoubleHallway(Rooms[i].R_Pos, Rooms[nextRoom].R_Pos, DoorWidth, DoorHeight);
         }
         //================================
 
@@ -121,11 +104,12 @@ public class RoomGenerator : IGenerator
         
     }
 
-    private void GenerateSecondaryRooms(Texture2D _texture)
+    private void GenerateSecondaryRooms(Texture2D _texture, int _padding, int _hallwaySearchDist)
     {
-        for (int i = 0; i < maxNumOfSecondaryRooms; i++)
+        int successfulRooms = 0;
+        for (int i = 0; i < 1000; i++)
         {
-             RoomMarker tempRoom = new RoomMarker();
+            RoomMarker tempRoom = new RoomMarker();
             tempRoom.R_Size = new Vector2Int((int)Random.Range(minRoomSize.x, maxRoomSize.x), (int)Random.Range(minRoomSize.y, maxRoomSize.y));
             Vector2Int Padding = new Vector2Int((tempRoom.R_Size.x / 2), (tempRoom.R_Size.y / 2));
             tempRoom.R_Pos = new Vector2Int((int)Random.Range(0 + Padding.x, _texture.Size().x - Padding.x), (int)Random.Range(0 + Padding.y, _texture.Size().y - Padding.y));
@@ -133,32 +117,95 @@ public class RoomGenerator : IGenerator
 
             bool success = true;
 
-            for (int x = 0; x < tempRoom.R_Size.x; x++)
+            for (int x = 0 - _padding; x < tempRoom.R_Size.x + _padding; x++)
             {
-                for (int y = 0; y < tempRoom.R_Size.y; y++)
+                for (int y = 0 - _padding; y < tempRoom.R_Size.y + _padding; y++)
                 {
                     Vector2Int _pos = new Vector2Int(tempRoom.R_Pos.x + x - offset.x, tempRoom.R_Pos.y + y - offset.y);
-                    if(roomTexture.GetPixel(_pos.x, _pos.y) != Color.black)
+                    if (roomTexture.GetPixel(_pos.x, _pos.y) != Color.black)
                     {
                         success = false;
                     }
                 }
                 if (!success) break;
             }
+
+
+
+
+
+
+            Vector2Int nearestPos;
             if (success)
             {
+                if (GetNearestPPoint(out nearestPos, tempRoom.R_Pos, _texture, 100))
+                {
+                    DrawDoubleHallway(tempRoom.R_Pos, nearestPos, DoorWidth, DoorHeight);
+
+                }
                 for (int x = 0; x < tempRoom.R_Size.x; x++)
                 {
                     for (int y = 0; y < tempRoom.R_Size.y; y++)
                     {
+                       
                         roomTexture.SetPixel(tempRoom.R_Pos.x + x - offset.x, tempRoom.R_Pos.y + y - offset.y, Color.white);
+
                     }
                 }
+                successfulRooms++;
             }
-
-
+            if (successfulRooms >= maxNumOfSecondaryRooms) break;
         }
 
+    }
+    private bool GetNearestPPoint(out Vector2Int _res, Vector2Int _origin, Texture2D _texture, int _searchDist)
+    {
+
+        for (int xy = 0; xy < _searchDist; xy++)
+        {
+            //Positive x search
+            Vector2Int pxs = new Vector2Int(xy, _origin.y);
+            if (pxs.x <= _texture.width - 1)
+            {
+                if (_texture.GetPixel(pxs.x, pxs.y) != Color.black)
+                {
+                    _res = new Vector2Int( pxs.x + 1, pxs.y);
+                    return true;
+                }
+            }
+            //Negative x search
+            Vector2Int nxs = new Vector2Int(-xy, _origin.y);
+            if (nxs.x >= 0)
+            {
+                if (_texture.GetPixel(nxs.x, nxs.y) != Color.black)
+                {
+                    _res = new Vector2Int(nxs.x - 1, nxs.y);
+                    return true;
+                }
+            }
+            //Positive y search
+            Vector2Int pys = new Vector2Int(_origin.x, xy);
+            if (pys.y <= _texture.height - 1)
+            {
+                if (_texture.GetPixel(pys.x, pys.y) != Color.black)
+                {
+                    _res = new Vector2Int(pys.x, pys.y + 1);
+                    return true;
+                }
+            }
+            //Negative y search
+            Vector2Int nys = new Vector2Int(_origin.x, -xy);
+            if (nys.y >= 0)
+            {
+                if (_texture.GetPixel(nys.x, nys.y) != Color.black)
+                {
+                    _res = new Vector2Int(nys.x, nys.y - 1);
+                    return true;
+                }
+            }
+        }
+        _res = Vector2Int.zero;
+        return false;
     }
     private RoomMarker[] GeneratePrimaryRooms(Texture2D _texture)
     {
@@ -203,22 +250,26 @@ public class RoomGenerator : IGenerator
 
         return new Vector2Int(xDist, yDist);
     }
-    private void DrawLine(Vector2Int _p1, Vector2Int _p2, int width)
+   
+    private void DrawDoubleHallway(Vector2Int _p1, Vector2Int _p2, int width, int height)
     {
         bool flipDir = Random.Range(0,2) == 0 ? true : false;
         //==============================
         //Draw Line One
         Vector2Int CursorPos = _p1;
         int safety = 1000;
-
+        float GSC = (float)height / maxHeight;
+        //Debug.Log("Width: " + width + "Floor: " + Mathf.FloorToInt(((float)(width - 1) / 2)) + " Ceiling: " + Mathf.CeilToInt(((float)(width - 1) / 2)));
         while (CursorPos != _p2 || safety <= 0)
         {
-            for (int x = CursorPos.x - Mathf.FloorToInt(((width - 1) / 2)) - 1; x < CursorPos.x + Mathf.CeilToInt(((width - 1) / 2)) + 1; x++)
+            for (int x = CursorPos.x - Mathf.FloorToInt((float)(width - 1) / 2) - 1; x < CursorPos.x + Mathf.CeilToInt((float)(width - 1) / 2) + 1; x++)
             {
                 for (int y = CursorPos.y - Mathf.FloorToInt(((width - 1) / 2)) - 1; y < CursorPos.y + Mathf.CeilToInt(((width - 1) / 2)) + 1; y++)
                 {
-
-                    roomTexture.SetPixel(x, y, Color.white);
+                    if (roomTexture.GetPixel(x, y) == Color.black)
+                    {
+                        roomTexture.SetPixel(x, y, new Color(GSC, GSC, GSC));
+                    }
                 }
             }
 
