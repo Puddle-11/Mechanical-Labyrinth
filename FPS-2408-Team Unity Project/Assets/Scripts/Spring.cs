8217cd6b79102d7118d89e8fd1a4cf17fa311c39
Tsunamibowl <DAHolness@student.fullsail.edu>
2024-08-28 22:10:01 -0400

    added more Shoot sounds

--- FPS-2408-Team Unity Project/Assets/Scripts/Spring.cs
+++ FPS-2408-Team Unity Project/Assets/Scripts/Spring.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class Spring : MonoBehaviour
 {
+    [SerializeField] Vector3 SpringVelocity;
     [SerializeField] Vector3 SpringDirection;
     [SerializeField] float Springforce;
     bool onspring;
-
+ 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
@@ -18,18 +19,16 @@ public class Spring : MonoBehaviour
             if (other.GetComponent<PlayerController>() != null)
             {
                 GameManager.instance.playerControllerRef.SetPlayervel(SpringDirection.normalized * Springforce);
-
+                Vector3.Reflect(SpringDirection.normalized * Springforce, SpringVelocity);
             }
-           else {
+        }
+           else 
+           {
             onspring = false;
            }
-        }
-        
-
-
     }
-
-
+    
+    
     // Update is called once per frame
     void Update()
     {
