b713a4d750259014e571e4aedde603ad44d97988
Muhanad Ibrahim <155984441+Muhanad69@users.noreply.github.com>
2024-09-06 13:53:27 -0400

    commented out the temp errors
    Commented out the errors from WIP code in general inventory.

--- FPS-2408-Team Unity Project/Assets/Scripts/GeneralInventory.cs
+++ FPS-2408-Team Unity Project/Assets/Scripts/GeneralInventory.cs
@@ -19,68 +19,70 @@ public class GeneralInventory : MonoBehaviour
     [Header("Inventory Variables")]
     [Space]
     [SerializeField] private int numOfslots;
+
+
     public Itemslot[] Hotbar = new Itemslot[10];
 
     // Start is called before the first frame update
-    void Awake()
-    {
-        if (instance == null)
-            instance = this;
-        else
-            Destroy(this);
+    //void Awake()
+    //{
+    //    if (instance == null)
+    //        instance = this;
+    //    else
+    //        Destroy(this);
 
-    }
-    private void Start()
-    {
-        if (GameManager.instance != null)
-        {
-            int[] temp = GameManager.instance.GetAmmoInventory();
-            if (temp != null && temp.Length > 0)
-            {
-                for (int i = 0; i < temp.Length; i++)
-                {
-                    if (i >= ammoCounts.Length) break;
-                    ammoCounts[i] = temp[i];
-                }
-            }
-        }
+    //}
+    //private void Start()
+    //{
+    //    if (GameManager.instance != null)
+    //    {
+    //        int[] temp = GameManager.instance.GetAmmoInventory();
+    //        if (temp != null && temp.Length > 0)
+    //        {
+    //            for (int i = 0; i < temp.Length; i++)
+    //            {
+    //                if (i >= ammoCounts.Length) break;
+    //                ammoCounts[i] = temp[i];
+    //            }
+    //        }
+    //    }
 
-    }
-    public int GetAmmoTypeCount()
-    {
-        return numOfAmmoTypes;
-    }
-    // Update is called once per frame
+    //}
+    //public int GetAmmoTypeCount()
+    //{
+    //    return numOfAmmoTypes;
+    //}
+    //// Update is called once per frame
 
-    public int GetAmmoAmount(bulletType type)
-    {
-        return GetAmmoAmount((int)type);
-    }
-    public int GetAmmoAmount(int _index)
-    {
-        return ammoCounts[_index];
+    //public int GetAmmoAmount(bulletType type)
+    //{
+    //    return GetAmmoAmount((int)type);
+    //}
+    //public int GetAmmoAmount(int _index)
+    //{
+    //    return ammoCounts[_index];
 
-    }
-    public Sprite GetAmmoIcon(bulletType type)
-    {
-        return GetAmmoIcon((int)type);
-    }
-    public Sprite GetAmmoIcon(int _index)
-    {
-        if (_index < typeIcons.Length)
-        {
-            return typeIcons[_index];
-        }
-        else
-        {
-            return null;
-        }
+    //}
+    //public Sprite GetAmmoIcon(bulletType type)
+    //{
+    //    return GetAmmoIcon((int)type);
+    //}
+    //public Sprite GetAmmoIcon(int _index)
+    //{
+    //    if (_index < typeIcons.Length)
+    //    {
+    //        return typeIcons[_index];
+    //    }
+    //    else
+    //    {
+    //        return null;
+    //    }
 
-    }
-    public void UpdateAmmoInventory(bulletType type, int amount)
-    {
-        ammoCounts[(int)type] += amount;
-        UIManager.instance.UpdateInternalAmmoInv(type);
-        if (GameManager.instance != null) GameManager.instance.SetAmmoInventory(ammoCounts);
-    }
+    //}
+    //public void UpdateAmmoInventory(bulletType type, int amount)
+    //{
+    //    ammoCounts[(int)type] += amount;
+    //    UIManager.instance.UpdateInternalAmmoInv(type);
+    //    if (GameManager.instance != null) GameManager.instance.SetAmmoInventory(ammoCounts);
+    //}
 }
