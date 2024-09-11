cd29cbc9c7080beda4af495aec2e338fe40d38b5
Tsunamibowl <DAHolness@student.fullsail.edu>
2024-09-10 22:58:52 -0400

    Health Pad implemented

---
+++ FPS-2408-Team Unity Project/Assets/Scripts/Enviornmental/Health Pad.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPad : MonoBehaviour
+{
+    [SerializeField] int HealAmount;
+    [SerializeField] GameObject pad;
+    //Color color;
+    bool OnPad = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == GameManager.instance.playerRef)
+        {
+            OnPad = true;
+            HealPlayer();
+            FlashGreen();
+        }
+    }
+    void HealPlayer() {
+        GameManager.instance.playerControllerRef.UpdateHealth(HealAmount);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator FlashGreen() {
+        pad.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        pad.SetActive(false);
+    }
+}
