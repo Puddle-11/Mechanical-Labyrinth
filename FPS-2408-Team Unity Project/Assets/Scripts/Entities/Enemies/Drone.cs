4cd5698082e8c678713beb5161e5177a9b30e0a7
Tsunamibowl <DAHolness@student.fullsail.edu>
2024-09-11 16:14:57 -0400

    Gave drones knockback

--- FPS-2408-Team Unity Project/Assets/Scripts/Entities/Enemies/Drone.cs
+++ FPS-2408-Team Unity Project/Assets/Scripts/Entities/Enemies/Drone.cs
@@ -8,12 +8,14 @@ using UnityEngine;
 public class Drone : BaseEnemy
 {
     [SerializeField] private int explosionDamage;
+    [SerializeField] private Vector3 knockback;
     #region Monobehavior Methods
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == GameManager.instance.playerRef)
         {
             GameManager.instance.playerControllerRef.UpdateHealth(-explosionDamage);
+            GameManager.instance.playerControllerRef.SetForce(knockback);
             base.Death();
         }
     }
