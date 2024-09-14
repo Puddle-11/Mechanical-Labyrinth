7faa53930c2b6166f2eda3116f8ac4ba5eacb0ce
Tsunamibowl <DAHolness@student.fullsail.edu>
2024-09-13 21:06:58 -0400

    reworked knockback on the drone enemy

--- FPS-2408-Team Unity Project/Assets/Scripts/Entities/Enemies/Drone.cs
+++ FPS-2408-Team Unity Project/Assets/Scripts/Entities/Enemies/Drone.cs
@@ -9,13 +9,15 @@ public class Drone : BaseEnemy
 {
     [SerializeField] private int explosionDamage;
     [SerializeField] private Vector3 knockback;
+    [SerializeField] private float knockbackmod;
     #region Monobehavior Methods
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == GameManager.instance.playerRef)
-        {
+        { 
+            knockback = GameManager.instance.playerRef.transform.position - transform.position;
             GameManager.instance.playerControllerRef.UpdateHealth(-explosionDamage);
-            GameManager.instance.playerControllerRef.SetForce(knockback);
+            GameManager.instance.playerControllerRef.SetForce(knockback.normalized * knockbackmod);
             base.Death();
         }
     }
