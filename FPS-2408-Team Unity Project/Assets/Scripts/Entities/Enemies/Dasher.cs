e7169e67b231faadf1f57b4986c42e18ef3513ee
Tsunamibowl <DAHolness@student.fullsail.edu>
2024-09-14 01:02:03 -0400

    Implemented dasher and updated exploding drone prefab

--- FPS-2408-Team Unity Project/Assets/Scripts/Entities/Enemies/Dasher.cs
+++ FPS-2408-Team Unity Project/Assets/Scripts/Entities/Enemies/Dasher.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Dasher : BaseEnemy
@@ -7,16 +8,29 @@ public class Dasher : BaseEnemy
     [SerializeField] int DashSpeed;
     [SerializeField] int RamDamage;
     [SerializeField] Vector3 Knockback;
+    [SerializeField] private float knockbackmod;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) {
-            Debug.Log("Hit Player");
+        if (other.gameObject == GameManager.instance.playerRef) {
+            Knockback = GameManager.instance.playerRef.transform.position - transform.position;
             GameManager.instance.playerControllerRef.UpdateHealth(-RamDamage);
-            GameManager.instance.playerControllerRef.GetForce();
-            GameManager.instance.playerControllerRef.SetForce(Knockback.normalized);
+            GameManager.instance.playerControllerRef.SetForce(Knockback.normalized * knockbackmod);
         }
     }
+    public override void Update()
+    {
+        StartCoroutine(DashTowardsPlayer());
+        base.Update();
+    }
+    IEnumerator DashTowardsPlayer() {
+        Vector3 directiontoplayer = GameManager.instance.playerRef.transform.position - transform.position;
+        Vector3 Direction = directiontoplayer / directiontoplayer.magnitude;
+        while (currState == EnemyState.Attack)
+        {
+            yield return new WaitForSeconds(5);
+            transform.Translate(directiontoplayer.normalized * DashSpeed * Time.deltaTime, Space.World);
 
-
+        }
+    }
 
 }
