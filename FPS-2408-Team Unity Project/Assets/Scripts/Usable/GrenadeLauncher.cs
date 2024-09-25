0348ec16ae03aa21efbb2e4face088ccfbd131bb
Tsunamibowl <DAHolness@student.fullsail.edu>
2024-09-25 14:09:57 -0400

    readded shop text

--- FPS-2408-Team Unity Project/Assets/Scripts/Usable/GrenadeLauncher.cs
+++ FPS-2408-Team Unity Project/Assets/Scripts/Usable/GrenadeLauncher.cs
@@ -7,11 +7,12 @@ public class GrenadeLauncher : BaseGun
     [SerializeField] private GameObject grenadeobject;
     [SerializeField] private float Projectilespeed;
     [SerializeField] private Transform shootpos;
+    
     public override IEnumerator AttackDelay() {
         isAttacking = true;
-        GameObject currgrenade = Instantiate(grenadeobject, shootpos.position, Quaternion.identity);
-        Rigidbody rb = currgrenade.GetComponent<Rigidbody>();
-        Vector3 Addedforce = Vector3.forward * Projectilespeed;
+        GameObject grenaderef = Instantiate(grenadeobject, shootpos.position, Quaternion.identity);
+        Rigidbody rb = grenaderef.GetComponent<Rigidbody>();
+        Vector3 Addedforce = shootpos.position.normalized * Projectilespeed;
         rb.AddForce(Addedforce, ForceMode.Impulse);
         yield return new WaitForSeconds(coolDown);
         isAttacking = false;
--- FPS-2408-Team Unity Project/Assets/Scripts/Weapons/Grenade.cs
+++ FPS-2408-Team Unity Project/Assets/Scripts/Weapons/Grenade.cs
@@ -6,17 +6,53 @@ public class Grenade : MonoBehaviour
 {
     [SerializeField] int Explosiondamage;
     [SerializeField] float radius;
+    [SerializeField] GameObject grenade;
     Collider[] colliderref;
+    [SerializeField] private float timer;
+    [SerializeField] private float maxtimer;
+
+    private void Start()
+    {
+        timer = maxtimer;
+    }
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Explosion();
+    }
     private void Explosion()
     {
-        IHealth healthref;
         colliderref = Physics.OverlapSphere(transform.position, radius);
-        foreach (Collider coll in colliderref) {
-            if (coll.gameObject.TryGetComponent(out healthref)) {
-                healthref.SetHealth(-Explosiondamage);
+        IHealth healthref;
+        for (int i = 0; i < colliderref.Length; i++)
+        {
+            for (int j = 0; j < colliderref.Length; j++)
+            {
+                if (colliderref[i].gameObject == gameObject)
+                {
+                    return;
+                }
+                else
+                {
+                    if (colliderref[i].GetComponent<BaseEnemy>().GetCurrentShield() > 0 || colliderref[i].GetComponent<BaseEnemy>().GetCurrentHealth() > 0)
+                    {
+                        Debug.Log("hit enemy");
+                        if (colliderref[i].GetComponent<BaseEnemy>().GetCurrentShield() <= 0)
+                        {
+                            colliderref[i].GetComponent<BaseEnemy>().SetCurrentShield(-Explosiondamage);
+                            Destroy(grenade);
+                        }
+                        if (colliderref[i].GetComponent<BaseEnemy>().GetCurrentHealth() <= 0)
+                        {
+                            colliderref[i].GetComponent<BaseEnemy>().SetHealth(-Explosiondamage);
+                            Destroy(grenade);
+                        }
+                    }
+                }
             }
         }
-        Destroy(gameObject);
-    } 
-    
+    }
 }
